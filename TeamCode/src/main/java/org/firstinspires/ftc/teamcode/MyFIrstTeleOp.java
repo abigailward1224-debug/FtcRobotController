6400639c9111cfa96f8ef1a3cee7e5ef054d0712
ΛW_nova <abigailward1224@gmail.com>
2026-06-15 17:16:37 -0400

    UpdatedDriveTrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIrstTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIrstTeleOp.java
@@ -5,17 +5,34 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name="My First TeleOp")
 public class MyFIrstTeleOp extends LinearOpMode{
-    private DcMotor leftDrive;
+    private DcMotor frontLeft;
+    private DcMotor backLeft;
+    private DcMotor frontRight;
+    private DcMotor backRight;
 
 @Override
 public void runOpMode() {
 
-    leftDrive = hardwareMap.get(DcMotor.class, "left_motor");
+    frontLeft  = hardwareMap.get(DcMotor.class, "front_left_motor");
+    backLeft   = hardwareMap.get(DcMotor.class, "back_left_motor");
+    frontRight = hardwareMap.get(DcMotor.class, "front_right_motor");
+    backRight  = hardwareMap.get(DcMotor.class, "back_right_motor");
+
+    frontLeft.setDirection(DcMotor.Direction.REVERSE);
+    backLeft.setDirection(DcMotor.Direction.REVERSE);
 
     waitForStart();
 
     while (opModeIsActive()) {
-        leftDrive.setPower(-gamepad1.left_stick_y);
+        double y = -gamepad1.left_stick_y; // Forward/back
+        double x = -gamepad1.left_stick_x;  // Strafing (sideways)
+        double rx = -gamepad1.right_stick_x; // Turning
+
+        // 2. Run the Mecanum Mix formula to combine the vectors
+        frontLeft.setPower(y + x + rx);
+        backLeft.setPower(y - x + rx);
+        frontRight.setPower(y - x - rx);
+        backRight.setPower(y + x - rx);
     }
 }
 }
