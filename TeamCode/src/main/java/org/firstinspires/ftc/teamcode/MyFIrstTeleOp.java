a346a30565fb5b7b43f885e406264a7c93152803
ΛW_nova <abigailward1224@gmail.com>
2026-07-07 14:47:35 -0400

    Update MyFIrstTeleOp.java
    added motorized intake control and logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIrstTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIrstTeleOp.java
@@ -5,10 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name="My First TeleOp")
 public class MyFIrstTeleOp extends LinearOpMode{
+
     private DcMotor frontLeft;
     private DcMotor backLeft;
     private DcMotor frontRight;
     private DcMotor backRight;
+
+    private DcMotor intake;
 
 @Override
 public void runOpMode() {
@@ -17,6 +20,7 @@ public void runOpMode() {
     backLeft   = hardwareMap.get(DcMotor.class, "back_left_motor");
     frontRight = hardwareMap.get(DcMotor.class, "front_right_motor");
     backRight  = hardwareMap.get(DcMotor.class, "back_right_motor");
+    intake = hardwareMap.get(DcMotor.class, "intake_motor");
 
     frontLeft.setDirection(DcMotor.Direction.REVERSE);
     backLeft.setDirection(DcMotor.Direction.REVERSE);
@@ -24,6 +28,7 @@ public void runOpMode() {
     waitForStart();
 
     while (opModeIsActive()) {
+
         double y = -gamepad1.left_stick_y; // Forward/back
         double x = -gamepad1.left_stick_x;  // Strafing (sideways)
         double rx = -gamepad1.right_stick_x; // Turning
@@ -33,6 +38,15 @@ public void runOpMode() {
         backLeft.setPower(y - x + rx);
         frontRight.setPower(y - x - rx);
         backRight.setPower(y + x - rx);
+
+        //intake logic
+        if (gamepad1.right_bumper) {
+            intake.setPower(1.0);  // Spin inward at full speed
+        } else if (gamepad1.left_bumper) {
+            intake.setPower(-1.0); // Spin outward at full speed
+        } else {
+            intake.setPower(0.0);  // Stop spinning when no buttons are pressed
+        }
     }
 }
 }
